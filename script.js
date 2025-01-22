325f0f87e05edb518b682e6b19cf4185651c9c6d
Jason Em <jasonem@umail.ucsb.edu>
2025-01-21 16:47:08 -0800

    Add playRound function
    Created function which uses getHumanChoice() and getComputerChoice()
    to decide who wins and increment winner's global point variable.
    Also added some cgecks at start of function to ensure human choice
    is valid answer.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let computerChoice = Math.floor((Math.random() * 3) + 1);
     if (computerChoice == 1){
@@ -13,3 +16,45 @@ function getHumanChoice() {
     let humanChoice = prompt("Will you choose rock, paper, or scissor?").toLowerCase();
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    let outcome;
+    if (humanChoice == computerChoice){
+        return "Tie. No one scores."
+    } 
+    if (humanChoice == "rock"){
+        if (computerChoice == "paper"){
+            outcome = 0;
+        } else{
+            outcome = 1;
+        }
+    } else if (humanChoice == "paper"){
+        if (computerChoice == "scissor"){
+            outcome = 0;
+        } else{
+            outcome = 1;
+        }
+    } else if (humanChoice == "scissor"){
+        if (computerChoice == "rock"){
+            outcome = 0;
+        } else{
+            outcome = 1;
+        }
+    } else{
+        return "Invalid answer. No one scores."
+    }
+    if (outcome == 0){
+        computerScore++;
+        return "You lose. " + computerChoice + " beats " + humanChoice;
+    } else {
+        humanScore++;
+        return "You win! " + humanChoice + " beats " + computerChoice;
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log("Human score: " + humanScore);
+console.log("Computer score: " + computerScore);
